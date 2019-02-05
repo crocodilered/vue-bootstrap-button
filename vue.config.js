770f99d5e413163c1695737ecb6c32d3b8dfe948
Serge <serge.zolotukhin@gmail.com>
2019-02-05 22:15:36 +0300

    Docs updated.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'vue-bootstrap-button/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : 'vue-bootstrap-button/'
 }
